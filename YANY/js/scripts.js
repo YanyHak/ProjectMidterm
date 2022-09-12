486ec5b3084c0db539426bf671ff90115abbd47f
Yany <jimititi999@gmail.com>
2022-09-12 17:05:48 +0700

    Project Midterm

---
+++ YANY/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.5 
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT 
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
